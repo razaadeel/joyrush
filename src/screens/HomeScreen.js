b888eb08c77f6692ec204c9421db08a0a4eb4bea
razaadeel8 <razaadeel8@gmail.com>
2020-11-01 15:58:17 +0500

    test

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 import Map from '../components/home/Map';
 
 const HomeScreen = () => {
+    let text = Platform.OS === 'android' ? 'android' : 'ios'
     return (
         <View style={styles.container}>
             <Map />
+            <Text>{text}</Text>
         </View>
     )
 }
